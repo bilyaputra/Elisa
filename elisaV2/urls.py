a83457a4b3c4cb579553df978f21153eab7f4bd3
Ahmad Ashril Rizal <ashril.rizal@gmail.com>
2022-02-01 22:01:31 +0800

    Update urls.py
    Membuang path yang tidak digunakan

--- elisaV2/urls.py
+++ elisaV2/urls.py
@@ -1,27 +1,17 @@
-"""elisaV2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import Settings, settings
 from django.urls import path
-from elisaapp.views import *
+from elisaapp.views import login, input_surat, dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login),
-    path('input/', input),
-    path('surat/', surat),
     path('input_surat/', input_surat),
     path('dashboard/', dashboard),
-]
+    # path('arsip/', arsip)
+    # path('disposisi/', disposisi)
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
