505ac2cedfa6af0d63d2edc368f9edce3263f9df
bilyaputra <bilyaputra@gmail.com>
2022-02-14 16:20:50 +0800

    menambah halaman arsip

--- elisaV2/urls.py
+++ elisaV2/urls.py
@@ -3,11 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import Settings, settings
 from django.urls import path
-<<<<<<< HEAD
 from elisaapp.views import input_surat, dashboard, arsip
-=======
-from elisaapp.views import input_surat, dashboard
->>>>>>> e197216a58c52d9ce2d5bbfd784c9256c424cb29
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -16,11 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='dashboard'), name='logout'),
     path('input-surat/', input_surat, name='input_surat'),
     path('dashboard/', dashboard, name='dashboard'),
-<<<<<<< HEAD
     path('arsip/', arsip, name='arsip'),
-=======
-    # path('arsip/', arsip)
->>>>>>> e197216a58c52d9ce2d5bbfd784c9256c424cb29
     # path('disposisi/', disposisi)
 ] 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- elisaapp/views.py
+++ elisaapp/views.py
@@ -3,7 +3,6 @@ from elisaapp.models import Surat
 from elisaapp.forms import FormSurat
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-<<<<<<< HEAD
 
 # tes
 # def input(request):
@@ -42,50 +41,10 @@ def input_surat(request):
             form = FormSurat()
             pesan = "Data berhasil disimpan"
 
-=======
-
-# def input(request):
-#     noSurat = "Masukkan Nomor Surat"
-#     pic = "orang"
-#     noHP = "0812345678"
-#     tanggalSurat = "01-01-2022"
-#     tanggalMasuk = "02-01-2022"
-#     tanggalDeadline = "03-01-2022"
-#     perihal = "Undangan Wisuda"
-
-#     idSurat = {
-#         'noSurat' : noSurat,
-#         'pic' : pic,
-#         'tanggalSurat' : tanggalSurat,
-#         'tanggalMasuk' : tanggalMasuk,
-#         'tanggalDeadline' : tanggalDeadline,
-#         'perihal' : perihal
-
-#     }
-#     return render(request, 'input.html', idSurat)
-
-# def surat (request):
-#     surats = Surat.objects.all()
-#     konteks = {
-#         'surat' : surat
-#     }
-#     return render (request, 'surat.html', konteks)
-
-@login_required(login_url=settings.LOGIN_URL)
-def input_surat(request):
-    if request.POST:
-        form = FormSurat(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            form = FormSurat()
-            pesan = "Data berhasil disimpan"
-
->>>>>>> e197216a58c52d9ce2d5bbfd784c9256c424cb29
             konteks = {
                 'form' : form,
                 'pesan' : pesan,
             }
-<<<<<<< HEAD
 
             return render(request, 'input_surat.html', konteks)
     else:
@@ -115,18 +74,3 @@ def arsip(request):
     }
 
     return render(request, 'arsip.html', konteks)
-=======
-
-            return render(request, 'input_surat.html', konteks)
-    else:
-        form = FormSurat()
-
-        konteks = {
-            'form' : form,
-        }
-
-    return render(request,'input_surat.html', konteks)
-
-def dashboard(request):
-    return render(request, 'dashboard.html')
->>>>>>> e197216a58c52d9ce2d5bbfd784c9256c424cb29
